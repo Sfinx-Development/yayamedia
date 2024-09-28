d6b8af988860bed2a16f4ca3aa7f8684aa205edf
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-09-28 14:37:59 +0200

    första utkast av krysset

--- src/components/DrawerComponent.tsx
+++ src/components/DrawerComponent.tsx
@@ -7,17 +7,30 @@ export interface DrawerComponentProps {
     open: boolean
   ) => (event: React.KeyboardEvent | React.MouseEvent) => void;
 }
+
+const Texting = styled(Typography)`
+  font-family: "H2", sans-serif;
+  font-variation-settings: "wght" 300;
+`;
+
+const Atyp = styled(Typography)`
+  font-family: "AtypRegularVariable", sans-serif;
+  font-variation-settings: "wght" 300;
+`;
+
+const CrossLine = styled(Box)`
+  background-color: #363434;
+  height: 2px; // Justera höjden
+  width: 30px; // Justera bredden
+  margin: 0 2px;
+  transition: transform 0.3s ease;
+
+  &:hover {
+    cursor: pointer; // Markera att det är klickbart
+  }
+`;
+
 export default function DrawerComponent(props: DrawerComponentProps) {
-  const Texting = styled(Typography)`
-    font-family: "H2", sans-serif;
-    font-variation-settings: "wght" 300;
-  `;
-
-  const Atyp = styled(Typography)`
-    font-family: "AtypRegularVariable", sans-serif;
-    font-variation-settings: "wght" 300;
-  `;
-
   return (
     <Drawer
       anchor="right"
@@ -39,11 +52,31 @@ export default function DrawerComponent(props: DrawerComponentProps) {
           sx={{
             display: "flex",
             width: "100%",
-            justifyContent: "flex-end",
+            flexDirection: "column",
+            alignItems: "flex-end",
             paddingBottom: 4,
+            marginTop: 4,
           }}
         >
-          <Atyp fontSize={15}>Stäng</Atyp>
+          <CrossLine
+            sx={{
+              transform: "rotate(20deg) translateY(0px)",
+              marginBottom: "3px",
+              borderRadius: 20,
+              width: 35,
+              marginRight: 2,
+            }}
+          />
+          <CrossLine
+            sx={{
+              transform: "rotate(-15deg) translateY(-6px)",
+              width: 55,
+              borderRadius: 20,
+            }}
+          />
+          <Atyp fontSize={16} sx={{ marginRight: 1.5 }}>
+            Stäng
+          </Atyp>
         </Box>
         <Link href="/case" style={{ textDecoration: "none", color: "#363434" }}>
           <Texting sx={{ paddingY: 2, fontSize: 20 }}>Case</Texting>
@@ -58,31 +91,31 @@ export default function DrawerComponent(props: DrawerComponentProps) {
           href="/tjanster/socialamedier"
           style={{ textDecoration: "none", color: "#363434" }}
         >
-          <Atyp sx={{ fontSize: 15 }}>Sociala medier</Atyp>
+          <Atyp sx={{ fontSize: 16 }}>Sociala medier</Atyp>
         </Link>
         <Link
           href="/tjanster/webdesign"
           style={{ textDecoration: "none", color: "#363434" }}
         >
-          <Atyp sx={{ fontSize: 15 }}>Webdesign</Atyp>
+          <Atyp sx={{ fontSize: 16 }}>Webdesign</Atyp>
         </Link>
         <Link
           href="/tjanster/konceptutveckling"
           style={{ textDecoration: "none", color: "#363434" }}
         >
-          <Atyp sx={{ fontSize: 15 }}>Konceptutveckling</Atyp>
+          <Atyp sx={{ fontSize: 16 }}>Konceptutveckling</Atyp>
         </Link>
         <Link
           href="/tjanster/copywriting"
           style={{ textDecoration: "none", color: "#363434" }}
         >
-          <Atyp sx={{ fontSize: 15 }}>Copywriting</Atyp>
+          <Atyp sx={{ fontSize: 16 }}>Copywriting</Atyp>
         </Link>
         <Link
           href="/tjanster/grafisk-profil"
           style={{ textDecoration: "none", color: "#363434" }}
         >
-          <Atyp sx={{ paddingBottom: 0.5, fontSize: 15 }}>Grafisk profil</Atyp>
+          <Atyp sx={{ paddingBottom: 0.5, fontSize: 16 }}>Grafisk profil</Atyp>
         </Link>
         <Link
           href="/om-oss"
