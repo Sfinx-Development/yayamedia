a1bbb8f31d0d0196e7c03c4c2a8629f42a152c83
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-09-29 11:37:52 +0200

    green component in index

---
+++ src/components/GreenComponent.tsx
@@ -0,0 +1,57 @@
+import { Box, styled, Typography } from "@mui/material";
+
+export default function GreenComponent() {
+  const Texting = styled(Typography)`
+    font-family: "H3", sans-serif;
+    font-variation-settings: "wght" 300;
+  `;
+
+  const TextingATYP = styled(Typography)`
+    font-family: "AtypRegularVariable", sans-serif;
+    font-variation-settings: "wght" 300;
+  `;
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        background: "linear-gradient(to top, #B9DCD2, #90C9D3)",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "column" },
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          maxWidth: 900,
+          width: "100%",
+        }}
+      >
+        <Texting sx={{ fontSize: { xs: 40, md: 80 }, color: "#363434" }}>
+          Yttepytteliten.
+        </Texting>
+        <Texting sx={{ fontSize: { xs: 30, md: 50 }, color: "#363434" }}>
+          Vår byrå - inte vår vision.
+        </Texting>
+        <TextingATYP
+          sx={{
+            fontSize: { xs: 16, md: 20 },
+            fontWeight: "lighter",
+            color: "#363434",
+            flexWrap: "wrap",
+            maxWidth: 900,
+            marginTop: 1,
+          }}
+        >
+          Vi är en mindre reklambyrå på två som brinner för kreativt skapande.
+          Värdesätter du en personlig och nära kontakt, är Yaya media rätt för
+          dig. Vi ses över en kaffe, snackar gött och utvecklar strategier som
+          förverkligar dina visioner.
+        </TextingATYP>
+      </Box>
+    </Box>
+  );
+}
--- src/components/ParallaxWave.tsx
+++ src/components/ParallaxWave.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, styled, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import GreenComponent from "./GreenComponent";
 import Wave from "./Wave";
 
 export default function ParallaxWave() {
@@ -59,73 +60,79 @@ export default function ParallaxWave() {
             width: "100%",
             //   height: "100%",
             zIndex: 3, // Se till att in
-
             display: "flex",
-            flexDirection: "row",
+            flexDirection: "column",
             justifyContent: "center",
           }}
         >
           <Box
             sx={{
-              backgroundColor: "#F7F7F7",
-              margin: 0,
               display: "flex",
-              flexDirection: "column",
-              justifyContent: "flex-end",
-              // marginLeft: 15,
-              // flex: 1,
-              // display: "flex",
-              // flexDirection: "column",
-              // flex: 1,
-              // alignItems: "end",
+              flexDirection: "row",
+              justifyContent: "center",
             }}
           >
-            <Texting sx={{ fontSize: 45, color: "#363434", marginBottom: 2 }}>
-              Slut på moves?
-            </Texting>
-            <TextingATYP
+            <Box
               sx={{
-                width: 450,
+                backgroundColor: "#F7F7F7",
+                margin: 0,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-end",
+                paddingBottom: 20,
               }}
             >
-              Det brusar högt där ute. Att hitta sin unika rytm bland tusentals
-              andra kan vara svårt - vi vet. Men vi vet också hur man skapar
-              autentiskt innehåll. Du vet, sånt vi alla faktiskt frivilligt
-              konsumerar. Ibland är allt som behövs ett enkelt klick. Andra
-              gånger en skarp klack. Och för vissa, ett kraftfullt boom för att
-              verkligen bryta igenom bruset.
-            </TextingATYP>
-            <TextingATYP
-              sx={{
-                width: 450,
-                marginTop: 2,
-              }}
-            >
-              Oavsett hur du för dig just nu kan du räkna med oss! En
-              fullständig koreografi eller ett par väl valda moves? Upp till
-              dig! Vi erbjuder paketlösningar för det mesta, och anpassar oss
-              alltid efter dina behov.
-            </TextingATYP>
-            <Button
-              sx={{
-                backgroundColor: "#363434",
-                marginTop: 3,
-                maxWidth: "40%",
-                color: "#F7F7F7",
-                borderRadius: 2,
-                paddingY: 1,
-                textTransform: "none",
-              }}
-            >
-              <Texting sx={{ fontSize: 18 }}>Våra tjänster</Texting>
-            </Button>
+              <Texting sx={{ fontSize: 45, color: "#363434", marginBottom: 2 }}>
+                Slut på moves?
+              </Texting>
+              <TextingATYP
+                sx={{
+                  width: 450,
+                }}
+              >
+                Det brusar högt där ute. Att hitta sin unika rytm bland
+                tusentals andra kan vara svårt - vi vet. Men vi vet också hur
+                man skapar autentiskt innehåll. Du vet, sånt vi alla faktiskt
+                frivilligt konsumerar. Ibland är allt som behövs ett enkelt
+                klick. Andra gånger en skarp klack. Och för vissa, ett
+                kraftfullt boom för att verkligen bryta igenom bruset.
+              </TextingATYP>
+              <TextingATYP
+                sx={{
+                  width: 450,
+                  marginTop: 2,
+                }}
+              >
+                Oavsett hur du för dig just nu kan du räkna med oss! En
+                fullständig koreografi eller ett par väl valda moves? Upp till
+                dig! Vi erbjuder paketlösningar för det mesta, och anpassar oss
+                alltid efter dina behov.
+              </TextingATYP>
+              <Button
+                sx={{
+                  backgroundColor: "#363434",
+                  marginTop: 3,
+                  maxWidth: "40%",
+                  color: "#F7F7F7",
+                  borderRadius: 2,
+                  paddingY: 1,
+                  textTransform: "none",
+                }}
+              >
+                <Texting sx={{ fontSize: 18 }}>Våra tjänster</Texting>
+              </Button>
+            </Box>
+            <Box sx={{ marginTop: 1 }}>
+              <video width="640" height="400" autoPlay loop muted>
+                <source
+                  src="https://i.imgur.com/5yECUKT.mp4"
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
+            </Box>
           </Box>
-          <Box sx={{ marginTop: 1 }}>
-            <video width="640" height="400" autoPlay loop muted>
-              <source src="https://i.imgur.com/5yECUKT.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </Box>
+          <GreenComponent />
         </Box>
       </Box>
     </Box>
