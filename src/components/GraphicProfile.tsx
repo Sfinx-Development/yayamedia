685e6a9cc4a799dc86dc93c0be6561f3f7024ddd
Elina Kerola <elinak90@icloud.com>
2024-10-16 14:59:52 +0200

    marginbottom

--- src/components/GraphicProfile.tsx
+++ src/components/GraphicProfile.tsx
@@ -59,7 +59,7 @@ export default function GraphicProfile() {
           backgroundColor: "#F7F7F7",
           display: "flex",
           flexDirection: { xs: "row", md: "row" },
-          paddingBottom: { xs: 6, md: 10 },
+          paddingBottom: { xs: 8, md: 10 },
           margin: { xs: 0, md: 0 },
           //   height: "100%",
           justifyContent: "start",
