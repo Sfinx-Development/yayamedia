8d94b1af801039905679ee467c7e3eec1d110a6f
Elina Kerola <elinak90@icloud.com>
2025-05-04 20:56:36 +0200

    yaya första sida ny

--- src/components/ParallaxINDEX.tsx
+++ src/components/ParallaxINDEX.tsx
@@ -19,7 +19,7 @@ export default function ParallaxWave() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [scrollPosition, setScrollPosition] = useState(0);
   const isMobile = window.innerWidth <= 820;
-  const isBigScreen = window.innerHeight >= 1080;
+  // const isBigScreen = window.innerHeight >= 1080;
   useEffect(() => {
     const handleScroll = () => {
       setScrollPosition(window.scrollY);
@@ -81,6 +81,8 @@ export default function ParallaxWave() {
             position: "absolute",
             width: "100%",
             zIndex: 3,
+            paddingY: { xs: 6, md: 3 }, // vertikal padding
+            textAlign: "center", // centrera textinnehåll
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
@@ -115,107 +117,268 @@ export default function ParallaxWave() {
                 }}
                 component="h2"
               >
-                Slut på moves?
+                Sköna nyheter!
               </Texting>
               <TextingATYP
                 sx={{
-                  width: { xs: 320, md: 450, xl: 600 },
+                  width: { xs: 320, md: 650, xl: 1000 },
                   fontSize: isTablet ? 18 : { xl: 25 },
                 }}
                 component="h3"
               >
-                Det brusar högt där ute. Att hitta sin unika rytm bland
-                tusentals andra kan vara svårt - vi vet. Men vi vet också hur
-                man skapar autentiskt innehåll. Du vet, sånt vi alla faktiskt
-                frivilligt konsumerar. Ibland är allt som behövs ett enkelt
-                klick. Andra gånger en skarp klack. Och för vissa, ett
-                kraftfullt boom för att verkligen bryta igenom bruset.
+                Sommaren närmar sig och hela teamet glider snart in i
+                semestermode. Du däremot? Stirrar på ett tomt content-schema med
+                lätt panik i blicken. Eller så sitter du på en röst som fötjänar
+                mer - och är sugen på att äntligen sätta tänderna i en podd.
               </TextingATYP>
               <TextingATYP
                 sx={{
-                  width: { xs: 320, md: 450, xl: 600 },
+                  width: { xs: 320, md: 650, xl: 1000 },
                   marginTop: 2,
                   fontSize: isTablet ? 18 : { xl: 25 },
                 }}
                 component="h3"
               >
-                Oavsett hur du för dig just nu kan du räkna med oss! En
-                fullständig koreografi eller ett par väl valda moves? Upp till
-                dig! Vi erbjuder paketlösningar för det mesta, och anpassar oss
-                alltid efter dina behov.
+                Oavsett vilket - we got you. Vi har knåpat ihop två nya tjänster
+                som gör det enkelt att hålla kommunikationen levande, även när
+                resten av världen lutar sig tillbaka. Scrolla ned, spana in -
+                och häng på oss in i sommaren (och gärna in på hösten också!).
               </TextingATYP>
-              <Button
-                aria-label="Navigera till tjänster-sidan"
-                onClick={() => {
-                  navigate("/tjanster");
-                }}
-                sx={{
-                  backgroundColor: "#363434",
-                  marginTop: 3,
-                  maxWidth: isTablet ? "60%" : "40%",
-                  color: "#F7F7F7",
-                  borderRadius: 2,
-                  paddingY: { md: 1, xl: 2 },
-                  textTransform: "none",
-                  "&:hover": {
-                    background: "linear-gradient(to top, #EDC1D0, #F3D9DF)", // Färgen för hover-effekten
-                  },
-                  "&:active": {
-                    background: "linear-gradient(to top, #EDC1D0, #F3D9DF)", // Färgen när knappen är aktiv (om nödvändigt)
-                  },
-                }}
-              >
-                <Texting sx={{ fontSize: 18 }}>Våra tjänster</Texting>
-              </Button>
-            </Box>
-            <Box
-              sx={{
-                marginTop: { xs: 0, md: 2, xl: 8 },
-                backgroundColor: "#F7F7F7",
-                padding: 0,
-                margin: 0,
-                display: "flex",
-                alignItems: isTablet ? "end" : "start",
-              }}
-            >
-              <video
-                width={
-                  isMobile && !isTablet
-                    ? "0"
-                    : isTablet
-                    ? "350"
-                    : isBigScreen
-                    ? "700"
-                    : "500"
-                }
-                autoPlay
-                loop
-                muted
-                style={{ display: isMobile ? "none" : "block" }} // Döljer videon om enheten är mobil
-              >
-                {/* <video
-                width={
-                  isMobile && !isTablet
-                    ? "320"
-                    : isTablet
-                    ? "350"
-                    : isBigScreen
-                    ? "700"
-                    : "500"
-                }
-                // height="400"
-                autoPlay
-                loop
-                muted
-              > */}
-                <source
-                  src="https://i.imgur.com/5yECUKT.mp4"
-                  type="video/mp4"
-                />
-                Your browser does not support the video tag.
-              </video>
             </Box>
           </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              gap: 4,
+              alignItems: "stretch", // <- viktig
+              justifyContent: "center",
+              my: { xs: 4, md: 8 },
+              px: { xs: 2, sm: 4 },
+              width: "100%",
+              maxWidth: 1200,
+              mx: "auto",
+            }}
+          >
+            <Box
+              sx={{
+                minHeight: { xs: "auto", md: 600 },
+                flex: 1,
+                background: "linear-gradient(to top,  #FAECD0,#FAF7E4)",
+                width: { xs: "90%", sm: "90%", md: "45%" }, // samma regler
+                mx: "auto", // centrera på xs och sm
+                 py: 4,
+            
+                 pb: 4, // ← lite mindre padding än det gröna
+                px: { xs: 3, sm: 4, md: 5 },
+                borderRadius: 4,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                position: "relative",
+                overflow: "visible",
+                height: "100%", // <- lägg till denna
+              }}
+            >
+              <Box>
+                <Box sx={{ textAlign: "left", mt: 6, mb: 2 }}>
+                  <Texting
+                    component="h3"
+                    sx={{
+                      fontSize: { xs: 28, md: 32 },
+                      fontWeight: "bold",
+                      lineHeight: 1.3,
+                    }}
+                  >
+                    Klick, klack
+                  </Texting>
+                  <Texting
+                    component="h4"
+                    sx={{
+                      fontSize: { xs: 20, md: 22 },
+                      fontWeight: 300,
+                      lineHeight: 1.3,
+                    }}
+                  >
+                    (semester, tack!)
+                  </Texting>
+                </Box>
+
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: -160,
+                    left: "75%",
+                    transform: "translateX(-50%)",
+                    zIndex: 1,
+                  }}
+                >
+                  <img
+                    src="https://i.imgur.com/8XeB1Gl.png"
+                    alt="paraply och solstol"
+                    style={{ height: 370 }}
+                  />
+                </Box>
+
+                <TextingATYP
+                  sx={{   textAlign: "left",     
+                  fontSize: isTablet ? 18 : { xl: 25 }, }}
+                  component="p"
+                >
+                  Varvar du pitchar och paraplydrinkar med kpier och envis sand
+                  mellan tårna? Vi vet hur det är – alla vill ta sin semester,
+                  samtidigt. Men kommunikation står sällan still – inte ens när
+                  hela marknadsteamet drömmer om solstols-häng och iskall sorbet
+                  (eller öl, för den delen).
+                  <br />
+                  <br />
+                  Men du, andas ut!
+                  <br />
+                  <br />
+                  Låt oss hoppa in där det behövs – nyhetsbrev, some, copy eller
+                  större designprojekt. Ut i solen och njut, så håller vi
+                  ställningarna tills du är tillbaka. Deal?
+                  <br />
+                  <br />
+                  <strong>Klick, klack (semester, tack!)</strong>
+                  <br />
+                  <em>
+                    Hos Yaya hela sommaren (eller så långt solstolarna räcker).
+                  </em>
+                </TextingATYP>
+              </Box>
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#363434",
+                  marginTop: 1,
+                  mt: "auto", // flytta neråt
+                  // mb: 2,      // luft under
+                  // width: "35%",
+                  width: "fit-content",
+                  color: "#F7F7F7",
+                  borderRadius: 2,
+                  paddingY: 1.2,
+                  textTransform: "none",
+                  marginY: { xs: 2, md: 0 }, // ✅ matchar det gröna
+
+                  "&:hover": {
+                    backgroundColor: "#363434",
+                  },
+                }}
+              >
+                Semester, tack!
+              </Button>
+            </Box>
+
+            <Box
+              sx={{
+                flex: 1,
+                backgroundColor: "#D2EAE6",
+                // width: { xs: "90%", sm: "90%", md: "45%" }, // exakt samma
+                mx: "auto",
+                py: 4,
+                px: { xs: 3, sm: 4, md: 5 },
+                borderRadius: 4,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                position: "relative",
+                overflow: "visible",
+                height: "100%", // <- lägg till denna
+              }}
+            >
+              <Box>
+                <Box sx={{ textAlign: "left", mt: 6, mb: 2 }}>
+                  <Texting
+                    component="h3"
+                    sx={{
+                      fontSize: { xs: 28, md: 32 },
+                      fontWeight: "bold",
+                      lineHeight: 1.3,
+                    }}
+                  >
+                    Poddpaketet
+                  </Texting>
+                  <Texting
+                    component="h4"
+                    sx={{
+                      fontSize: { xs: 20, md: 22 },
+                      fontWeight: 300,
+                      lineHeight: 1.3,
+                    }}
+                  >
+                    Här för att hänga – länge!
+                  </Texting>
+                </Box>
+
+                <Box
+                  sx={{
+                    position: "absolute",
+                    bottom: -165,
+                    left: "68%",
+                    transform: "translateX(-50%)",
+                    zIndex: 1,
+                  }}
+                >
+                  <img
+                    src="https://i.imgur.com/Dz59thB.png"
+                    alt="mikrofon"
+                    style={{ height: 400 }}
+                  />
+                </Box>
+
+                <TextingATYP
+                  sx={{ fontSize: 16, mb: 3, textAlign: "left" }}
+                  component="p"
+                >
+                  För dig som vill kommunicera smart, ärligt och med närvaro –
+                  eller bara snacka lite planlöst.
+                  <br />
+                  <br />
+                  Kanske vill du dra igång en helt ny podd och tänker
+                  långsiktigt, eller så är du inne på att släppa ett enda
+                  avsnitt för en kampanj. Eller varför inte skapa ett snackigt
+                  format för interna och externa budskap? We got you – oavsett
+                  hur din dröm ser ut.
+                  <br />
+                  <br />
+                  Lansering, storytelling, stort eller smått – up to you. Vi
+                  står för teknik, format och produktion.
+                  <br />
+                  <br />
+                  Du står för rösten.
+                  <br />
+                  <br />
+                  <strong>Poddpaketet</strong>
+                  <br />
+                  <em>Här för att hänga – länge!</em>
+                </TextingATYP>
+              </Box>
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#2F5C4A",
+                  marginTop: 1,
+                  // width: "35%",
+                  width: "fit-content",
+                  color: "#F7F7F7",
+                  borderRadius: 2,
+                  mt: "auto", // flytta neråt
+                  mb: 2,      // luft under
+                  paddingY: 1.2,
+                  textTransform: "none",
+                  marginY: { xs: 2, md: 0 },
+                  "&:hover": {
+                    backgroundColor: "#363434",
+                  },
+                }}
+              >
+                Ja, starta podd!
+              </Button>
+            </Box>
+          </Box>
+
           <GreenComponent />
           <Case />
           <Partners />
