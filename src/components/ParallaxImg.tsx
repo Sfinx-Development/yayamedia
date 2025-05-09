cfbef29975ece9ada9e369e3ce315599614de608
Elina Kerola <elinak90@icloud.com>
2025-05-09 13:25:20 +0200

    asundsholm

--- src/components/ParallaxImg.tsx
+++ src/components/ParallaxImg.tsx
@@ -1,7 +1,67 @@
+// // import { Box } from "@mui/material";
+// // import Wave from "./Wave";
+
+// // const ParallaxImg = () => {
+// //   return (
+// //     <Box
+// //       sx={{
+// //         width: "100%",
+// //         height: "100vh",
+// //         position: "relative",
+// //         overflow: "hidden",
+        
+// //       }}
+// //     >
+// //       {/* Bakgrundsbilden – FIXERAD */}
+// //       <Box
+// //           //  width: "100%",
+// //           //  backgroundColor: "#363434",
+// //           //  display: "flex",
+// //           //  justifyContent: "start",
+// //           //  // paddingY: isTablet ? 30 : { xs: 1, md: 4, xl: 0 },
+// //           //  paddingX: { xs: 1 },
+// //           //  alignItems: { xs: "center", xl: "start" },
+// //           //  position: "fixed",
+// //           //  zIndex: 2,
+// //           //  height: "100%",
+// //         sx={{
+// //           position: "fixed",
+// //           top: 0,
+// //           left: 0,
+// //           width: "100%",
+// //           height: "100vh",
+// //           backgroundImage: "url('https://i.imgur.com/pDMM8Je.png')",
+// //           backgroundSize: "cover",
+// //           backgroundPosition: "center",
+// //           zIndex: 1,
+// //         }}
+// //       />
+
+// //       {/* Vågen – rör sig uppåt vid scroll */}
+// //       <Box
+// //         sx={{
+// //           position: "relative",
+// //           width: "100%",
+// //           paddingY: { xs: 40, md: 10 },
+// //           zIndex: 2,
+// //         }}
+// //       >
+// //         <Wave />
+// //       </Box>
+// //     </Box>
+// //   );
+// // };
+
+// // export default ParallaxImg;
 // import { Box } from "@mui/material";
 // import Wave from "./Wave";
+// // import Wave from "./Wave";
 
-// const ParallaxImg = () => {
+// interface ParallaxImgProps {
+//   image: string; // 👈 Tillåter olika bakgrundsbilder
+// }
+
+// const ParallaxImg: React.FC<ParallaxImgProps> = ({ image }) => {
 //   return (
 //     <Box
 //       sx={{
@@ -9,95 +69,36 @@
 //         height: "100vh",
 //         position: "relative",
 //         overflow: "hidden",
-        
 //       }}
 //     >
-//       {/* Bakgrundsbilden – FIXERAD */}
-//       <Box
-//           //  width: "100%",
-//           //  backgroundColor: "#363434",
-//           //  display: "flex",
-//           //  justifyContent: "start",
-//           //  // paddingY: isTablet ? 30 : { xs: 1, md: 4, xl: 0 },
-//           //  paddingX: { xs: 1 },
-//           //  alignItems: { xs: "center", xl: "start" },
-//           //  position: "fixed",
-//           //  zIndex: 2,
-//           //  height: "100%",
-//         sx={{
-//           position: "fixed",
-//           top: 0,
-//           left: 0,
-//           width: "100%",
-//           height: "100vh",
-//           backgroundImage: "url('https://i.imgur.com/pDMM8Je.png')",
-//           backgroundSize: "cover",
-//           backgroundPosition: "center",
-//           zIndex: 1,
-//         }}
-//       />
+//       {/* Bakgrundsbild – fixerad i viewporten */}
+     
+//           <Box
+//           sx={{
+//             position: "relative", // ✅ gör den till en del av layouten
+//             width: "100%",
+//             height: "100vh",
+//             backgroundImage: `url(${image})`,
+//             backgroundSize: "cover",
+//             backgroundPosition: "center",
+//             zIndex: 1,
+//           }}
+//         />
+        
+     
 
-//       {/* Vågen – rör sig uppåt vid scroll */}
-//       <Box
+//  <Box
 //         sx={{
-//           position: "relative",
-//           width: "100%",
-//           paddingY: { xs: 40, md: 10 },
-//           zIndex: 2,
+//           // position: "relative",
+//           // width: "100%",
+//           // paddingY: { xs: 40, md: 10 },
+//           // zIndex: 3,
 //         }}
 //       >
-//         <Wave />
-//       </Box>
-//     </Box>
+//         <Wave /> 
+//       </Box> 
+//     // </Box>
 //   );
 // };
 
 // export default ParallaxImg;
-import { Box } from "@mui/material";
-import Wave from "./Wave";
-
-interface ParallaxImgProps {
-  image: string; // 👈 Tillåter olika bakgrundsbilder
-}
-
-const ParallaxImg: React.FC<ParallaxImgProps> = ({ image }) => {
-  return (
-    <Box
-      sx={{
-        width: "100%",
-        height: "100vh",
-        position: "relative",
-        overflow: "hidden",
-      }}
-    >
-      {/* Bakgrundsbild – fixerad i viewporten */}
-      <Box
-        sx={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100vh",
-          backgroundImage: `url(${image})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          zIndex: 1,
-        }}
-      />
-
-      {/* Vågen överst
-      <Box
-        sx={{
-          position: "relative",
-          width: "100%",
-          paddingY: { xs: 40, md: 10 },
-          zIndex: 2,
-        }}
-      >
-        <Wave /> */}
-      </Box>
-    // </Box>
-  );
-};
-
-export default ParallaxImg;
